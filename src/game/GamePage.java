fec553d4ce21fdcd8b33e09283ebd9eddb690d05
Spencer Davis <SDavis13@winona.edu>
2015-04-16 18:55:02 -0500

    More attempted fixes.

--- src/game/GamePage.java
+++ src/game/GamePage.java
@@ -2,6 +2,7 @@ package game;
 
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.RenderingHints;
 import java.awt.event.*;
 import java.util.Timer;
 import java.util.TimerTask;
@@ -26,7 +27,11 @@ public class GamePage extends View{
 
     protected void paintComponent(Graphics g1){
         super.paintComponent(g1);
-        curLevel.render((Graphics2D)g1);
+        Graphics2D g = (Graphics2D)g1;
+        g.setRenderingHint(RenderingHints.KEY_ANTIALIASING,RenderingHints.VALUE_ANTIALIAS_ON);
+        curLevel.render(g);
+        
+        g.dispose();
     }
     
     public void startRender(){
