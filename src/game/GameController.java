180edb8f4f175577d13fe58039a322412043e1e2
Spencer Davis <SDavis13@winona.edu>
2015-04-21 15:43:59 -0500

    Contactlistener

--- src/game/GameController.java
+++ src/game/GameController.java
@@ -6,9 +6,13 @@ import java.util.TimerTask;
 
 import javax.swing.event.MouseInputAdapter;
 
+import org.jbox2d.callbacks.ContactImpulse;
+import org.jbox2d.callbacks.ContactListener;
+import org.jbox2d.collision.Manifold;
 import org.jbox2d.common.Vec2;
+import org.jbox2d.dynamics.contacts.Contact;
 
-public class GameController {
+public class GameController implements ContactListener{
     GamePage view;
     Level curLevel;
     Ball ball;
@@ -109,6 +113,29 @@ public class GameController {
                 state = GameState.READY; //TEMP...FOR TESTING
             }
         }
+    }
+
+    @Override
+    public void beginContact(Contact contact) {
+        Entity entity1 = (Entity)contact.getFixtureA().getUserData();
+        Entity entity2 = (Entity)contact.getFixtureB().getUserData();
+        entity1.hit(entity2);
+        entity2.hit(entity1);
+    }
+    @Override
+    public void endContact(Contact contact) {
+        // TODO Auto-generated method stub
+        
+    }
+    @Override
+    public void preSolve(Contact contact, Manifold oldManifold) {
+        // TODO Auto-generated method stub
+        
+    }
+    @Override
+    public void postSolve(Contact contact, ContactImpulse impulse) {
+        // TODO Auto-generated method stub
+        
     }
         
 }
