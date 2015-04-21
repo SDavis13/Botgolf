bb66c2fe95123bd4fff208523c4e5444a4e802dd
JoshK13 <jkepros13@winona.edu>
2015-04-21 17:53:35 -0500

    Bot images. WIP bot classes

---
+++ src/game/ExplosionBot.java
@@ -0,0 +1,16 @@
+package game;
+
+import org.jbox2d.collision.shapes.PolygonShape;
+import org.jbox2d.dynamics.BodyDef;
+import org.jbox2d.dynamics.FixtureDef;
+import org.jbox2d.dynamics.World;
+
+public class ExplosionBot extends Mob {
+
+	ExplosionBot(World world, BodyDef bd, FixtureDef fd, PolygonShape shape,
+			float gridScale) {
+		super(world, bd, fd, shape, gridScale);
+		// TODO Auto-generated constructor stub		
+	}
+
+}
