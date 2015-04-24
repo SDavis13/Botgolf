03d6fe3934ad135df469b43dc58ee284be1b820f
cswanson12 <cswanson12@winona.edu>
2015-04-24 14:56:57 -0500

    Added javadoc to gamespec and gamestate

--- src/game/GameState.java
+++ src/game/GameState.java
@@ -1,5 +1,13 @@
 package game;
 
+
+/**
+ * GameState represents the states of the game.
+ * 
+ * @authors     Spencer Davis, Josh Kepros, Josh McDermott, Chris Swanson
+ * @version     1.0
+ * @since       2015-04-21
+ */
 public enum GameState {
     INACTIVE, PAUSED, READY, GRAB, LAUNCH, MOBTURN, BUILD, WIN
 }
