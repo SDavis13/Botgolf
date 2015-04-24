38b61a732d28bd710433aad77e5cdc31f6f132df
cswanson12 <cswanson12@winona.edu>
2015-04-24 14:48:18 -0500

    added javadoc to createlevel

--- src/game/CreateLevel.java
+++ src/game/CreateLevel.java
@@ -9,6 +9,15 @@ import org.jbox2d.dynamics.BodyType;
 import org.jbox2d.dynamics.FixtureDef;
 import org.jbox2d.dynamics.World;
 
+/**
+ * This is where the level is created.  The walls, hole, and robots are 
+ * defined in this.
+ * 
+ * @authors     Spencer Davis, Josh Kepros, Josh McDermott, Chris Swanson
+ * @version     1.0
+ * @since       2015-04-21
+ * @implements 	LevelFactory
+ */
 public class CreateLevel implements LevelFactory {
     /**
      * Value between 0 and 1. Higher = bouncier
@@ -32,6 +41,11 @@ public class CreateLevel implements LevelFactory {
     public static final Vec2 HOLE_POSITION = new Vec2(4,96);
 
 
+    /**
+     * createLevel method creates a level to the specs defined.
+     * 
+     * @param	specs	specs that are passed to level in order to create
+     */
     @Override
     public Level createLevel(GameSpec specs){
 
@@ -69,6 +83,12 @@ public class CreateLevel implements LevelFactory {
         return theLevel;
     }
 
+    /**
+     * createBall method used to define variables of the Ball.
+     * 
+     * @param 		theWorld	world that ball is created in
+     * @return					the ball object created
+     */
     private Ball createBall(World theWorld){
         Ball ball;
         FixtureDef ballFix = new FixtureDef();
@@ -84,6 +104,12 @@ public class CreateLevel implements LevelFactory {
         return ball;
     }
 
+    /**
+     * createHole method used to create the physics for the Hole.
+     * 
+     * @param 	theWorld	world where hole is created in
+     * @return				the Hole object that is created
+     */
     private Hole createHole(World theWorld)
     {
         FixtureDef fd = new FixtureDef();
@@ -98,6 +124,17 @@ public class CreateLevel implements LevelFactory {
         return new Hole(theWorld, bd, fd);
     }
 
+    /**
+     * createWall method for creating a wall in the world.
+     * 
+     * @param theWorld		the world where wall is created
+     * @param posX			x coordinate of where wall starts
+     * @param posY			y coordinate of where wall starts
+     * @param halfWidth		half the width of wall
+     * @param halfHeight	half the height of the wall
+     * @param rotation		rotation of the position for wall
+     * @return				returns a wall object
+     */
     private Wall createWall(World theWorld, float posX, float posY, float halfWidth, float halfHeight, float rotation){
         PolygonShape shape = new PolygonShape();
         shape.setAsBox(halfWidth, halfHeight, new Vec2(0,0), rotation);
@@ -113,6 +150,16 @@ public class CreateLevel implements LevelFactory {
         return new Wall(theWorld, bd, fd, shape);
     }
 
+    /**
+     * createMob is the method used to create the robot in the world.
+     * 
+     * @param theWorld		the world where the robot is created
+     * @param posX			x coordinate of the robot position
+     * @param posY			y coordinate of the robot position
+     * @param halfWidth		half the width of the mob
+     * @param halfHeight	half the height of the mob
+     * @return				returns a Mob object to the world
+     */
     private Mob createMob(World theWorld, float posX, float posY, float halfWidth, float halfHeight){
         PolygonShape shape = new PolygonShape();
         shape.setAsBox(halfWidth, halfHeight);
@@ -128,6 +175,16 @@ public class CreateLevel implements LevelFactory {
         return new Mob(theWorld, bd, fd, shape, GRID_SIZE);
     }
 
+    /**
+     * createStanMob is creating the position of standard robot in world.
+     * 
+     * @param theWorld		the world where standard robot is defined
+     * @param posX			x coordinate of the standard robot
+     * @param posY			y coordinate of the standard robot
+     * @param halfWidth		half width of the standard robot
+     * @param halfHeight	half height of the standard robot
+     * @return				returns a standard robot object
+     */
     private StandardBot createStanMob(World theWorld, float posX, float posY, float halfWidth, float halfHeight){
         PolygonShape shape = new PolygonShape();
         shape.setAsBox(halfWidth, halfHeight);
@@ -143,6 +200,16 @@ public class CreateLevel implements LevelFactory {
         return new StandardBot(theWorld, bd, fd, shape, GRID_SIZE);
     }
 
+    /**
+     * createExpMob is creating the position of explosion robot in world.
+     * 
+     * @param theWorld		the world where explosion robot is defined
+     * @param posX			x coordinate of the explosion robot
+     * @param posY			y coordinate of the explosion robot
+     * @param halfWidth		half width of the explosion robot
+     * @param halfHeight	half height of the explosion robot
+     * @return				returns a explosion robot object
+     */
     private ExplosionBot createExpMob(World theWorld, float posX, float posY, float halfWidth, float halfHeight){
         PolygonShape shape = new PolygonShape();
         shape.setAsBox(halfWidth, halfHeight);
