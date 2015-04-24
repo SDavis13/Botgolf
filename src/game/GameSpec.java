f575238872450c9a42f5e30d6b0dcbad5d4f9ff4
cswanson12 <cswanson12@winona.edu>
2015-04-24 18:32:38 -0500

    javadoc for gamespec

--- src/game/GameSpec.java
+++ src/game/GameSpec.java
@@ -4,8 +4,8 @@ package game;
  * GameSpec represents specs for a new game that is created.
  * 
  * @authors     Spencer Davis, Josh Kepros, Josh McDermott, Chris Swanson
- * @version     1.0
- * @since       2015-04-21
+ * @version     2015-04-24
+ * @since       2015-04-24
  */
 public class GameSpec {
     boolean newGame;
@@ -13,6 +13,11 @@ public class GameSpec {
     String levelName;
     int levelNum;
 
+    /**
+     * First constructor for GameSpec.
+     * 
+     * @param levelName		String of level name passed
+     */
     GameSpec(String levelName){
         newGame = true;
         campaignName = null;
@@ -20,6 +25,14 @@ public class GameSpec {
         levelNum = 0;
     }
 
+    /**
+     * Second constructor for GameSpec with different signature.
+     * 
+     * @param newGame		Boolean of new game passed
+     * @param campaignName	String of campaign name passed
+     * @param levelName		String of level name passed
+     * @param levelNum		Integer of level number passed
+     */
     GameSpec(boolean newGame, String campaignName, String levelName, int levelNum){
         this.newGame = newGame;
         this.campaignName = campaignName;
