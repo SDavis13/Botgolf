e1eeaa0ae189a85ff16501804df496ef32e8065d
JoshK13 <jkepros13@winona.edu>
2015-04-24 17:06:05 -0500

    stuff

---
+++ src/game/PausePage.java
@@ -0,0 +1,19 @@
+package game;
+
+import java.awt.event.ActionEvent;
+
+public class PausePage extends View {
+	static PausePage page = new PausePage(Consts.PAUSE);
+	
+	protected PausePage(String name)
+	{
+		super(name);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
