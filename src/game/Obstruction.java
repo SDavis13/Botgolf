ea678cf941bdc4c4d94be578501443ec570ca2be
cswanson12 <cswanson12@winona.edu>
2015-04-24 15:28:36 -0500

    javadoc to Obstruction

--- src/game/Obstruction.java
+++ src/game/Obstruction.java
@@ -1,5 +1,12 @@
 package game;
 
+/**
+ * This is Obstruction enum class to choose between MOB and WALL.
+ * 
+ * @authors     Spencer Davis, Josh Kepros, Josh McDermott, Chris Swanson
+ * @version     1.0
+ * @since       2015-04-21
+ */
 public enum Obstruction {
     MOB, WALL
 }
