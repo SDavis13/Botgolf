c146fadba480f48c8e2f33c0174dea0880b6164d
cswanson12 <cswanson12@winona.edu>
2015-04-24 16:11:35 -0500

    javadoc for view.

--- src/game/View.java
+++ src/game/View.java
@@ -4,21 +4,44 @@ import java.awt.event.ActionListener;
 
 import javax.swing.JPanel;
 
+/**
+ * This class represents the abstract class of View.  This also
+ * extends the Jpanel and implements ActionListener.
+ * 
+ * @authors     Spencer Davis, Josh Kepros, Josh McDermott, Chris Swanson
+ * @version     1.0
+ * @since       2015-04-21
+ * @extends		JPanel
+ * @implements	ActionListener
+ */
 public abstract class View extends JPanel implements ActionListener{
     String name;
     Main frame;
     boolean active;
 
+    /**
+     * Constructor for View.
+     * 
+     * @param 	name	passed a name for for view
+     */
     protected View(String name){
         this.name = name;
         frame = Main.getInstance();
         active = false;
     }
 
+    /**
+     * Activate method for the view.
+     * 
+     * @param 	message		passed this object.
+     */
     public void activate(Object message){
         active = true;
     }
 
+    /**
+     * Deactivate method for the view.
+     */
     public void deactivate(){
         active = false;
     }
