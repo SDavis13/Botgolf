73238af7b94c2b05bdf30558e4aa1a802488ec6e
JoshMcD14 <4321joshmcdermott@gmail.com>
2015-04-24 16:53:30 -0500

    added robot sounds

--- src/game/Consts.java
+++ src/game/Consts.java
@@ -33,16 +33,18 @@ public class Consts {
                                 GAME        = "Game";
     
                                 //Sound names
-    public static final String[] SOUNDS = {"Score", "Wall1", "Wall2"};
+    public static final String[] SOUNDS = {"Score", "Wall1", "Wall2", "RobotBoom", "RobotMove"};
     
     public static final int SNDIDX_SCORE = 0,
     						SNDIDX_WALL1 = 1,
-    						SNDIDX_WALL2 = 2;
+    						SNDIDX_WALL2 = 2,
+    						SNDIDX_ROBOTBOOM = 3,
+    						SNDIDX_ROBOTMOVE = 4;
     
 
                                 //Sound locations
     public static final String[] SOUNDFILES = {SNDLOC+"score.wav", SNDLOC+"wall1.wav",
-    	SNDLOC+"wall2"};
+    	SNDLOC+"wall2.wav", SNDLOC+"robotboom.wav", SNDLOC+"robotmove.wav"};
                                 
     
     public static final int SCALE = 2000;
