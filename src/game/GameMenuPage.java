26c6b0f4a588baede95925fd0c191fb57c8e90de
JoshK13 <jkepros13@winona.edu>
2015-04-24 20:14:58 -0500

    Ingame menu stuff

--- src/game/GameMenuPage.java
+++ src/game/GameMenuPage.java
@@ -7,16 +7,16 @@ import javax.swing.*;
 
 public class GameMenuPage extends View {
 	static GameMenuPage page = new GameMenuPage(Consts.GAMEMENUPAGE);
+	JPanel pausePanel = new JPanel();
+    JButton restartButton = new JButton("Restart");
+    JButton exitMainButton = new JButton("Exit To Main Menu");
 
 	protected GameMenuPage(String name) {
         super(name);
         
         Insets insetDefinition = new Insets(15,20,15,20);
-        
-        JPanel pausePanel = new JPanel();
-        JButton restartButton = new JButton("Restart");
+                
         restartButton.setMargin(insetDefinition);
-        JButton exitMainButton = new JButton("Exit To Main Menu");
         exitMainButton.setMargin(insetDefinition);
 
         pausePanel = new javax.swing.JPanel();
@@ -24,26 +24,28 @@ public class GameMenuPage extends View {
         exitMainButton = new javax.swing.JButton();
 
         restartButton.setText("Restart");
+        restartButton.addActionListener(this);
         exitMainButton.setText("Exit To Main Menu");
+        exitMainButton.addActionListener(this);
 
-        javax.swing.GroupLayout pausePanelLayout = new javax.swing.GroupLayout(pausePanel);
+        GroupLayout pausePanelLayout = new GroupLayout(pausePanel);
         pausePanel.setLayout(pausePanelLayout);
         pausePanelLayout.setHorizontalGroup(
-            pausePanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, pausePanelLayout.createSequentialGroup()
+            pausePanelLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
+            .addGroup(GroupLayout.Alignment.TRAILING, pausePanelLayout.createSequentialGroup()
                 .addGap(45, 45, 45)
-                .addComponent(restartButton, javax.swing.GroupLayout.PREFERRED_SIZE, 140, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(restartButton, GroupLayout.PREFERRED_SIZE, 140, GroupLayout.PREFERRED_SIZE)
                 .addGap(30, 30, 30)
-                .addComponent(exitMainButton, javax.swing.GroupLayout.PREFERRED_SIZE, 240, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(exitMainButton, GroupLayout.PREFERRED_SIZE, 240, GroupLayout.PREFERRED_SIZE)
                 .addContainerGap(45, Short.MAX_VALUE))
         );
         pausePanelLayout.setVerticalGroup(
-            pausePanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, pausePanelLayout.createSequentialGroup()
+            pausePanelLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
+            .addGroup(GroupLayout.Alignment.TRAILING, pausePanelLayout.createSequentialGroup()
                 .addContainerGap(496, Short.MAX_VALUE)
-                .addGroup(pausePanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(restartButton, javax.swing.GroupLayout.PREFERRED_SIZE, 75, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(exitMainButton, javax.swing.GroupLayout.PREFERRED_SIZE, 75, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGroup(pausePanelLayout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                    .addComponent(restartButton, GroupLayout.PREFERRED_SIZE, 75, GroupLayout.PREFERRED_SIZE)
+                    .addComponent(exitMainButton, GroupLayout.PREFERRED_SIZE, 75, GroupLayout.PREFERRED_SIZE))
                 .addGap(54, 54, 54))
         );
         
@@ -56,6 +58,11 @@ public class GameMenuPage extends View {
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		
+		if(e.getSource() == restartButton){
+			frame.switchView(Consts.GAME, new GameSpec("Test"));
+        }
+        if(e.getSource() == exitMainButton){
+        	frame.switchView(Consts.MAIN, null);
+        }
 	}
 }
